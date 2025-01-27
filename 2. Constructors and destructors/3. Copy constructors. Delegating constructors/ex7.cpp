c3530760d4f88d6f9dd06c721174b5d7aa682f68
Isaykin Daniil Timofeevich <k0tk3y@ya.ru>
2025-01-27 16:22:21 +0300

    Complex number

---
+++ 2. Constructors and destructors/3. Copy constructors. Delegating constructors/ex7.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+class Complex
+{
+    short re{0}, im{0};
+
+public:
+
+    Complex(short re=0, short im=0) : re(re), im(im)
+        { }
+    
+    Complex(Complex& other) : re(abs(-other.re)), im(abs(-other.im))
+        { }
+
+    short get_real()
+        { return re; }
+    
+    short get_imag()
+        { return im; }
+
+};
+
+int main()
+{
+    Complex digit{-4, 7},
+        res{digit};
+
+    // __ASSERT_TESTS__
+
+    return 0;
+}
