521cc94c54547856a2bbf1d464d174349b0434de
idt5574 <k0tk3y@ya.ru>
2025-04-10 14:35:59 +0300

    Custom exception class (ArrayException)

---
+++ 5. Exeption handling. Type conversion operations/5. Exception type identification. Custom exceptions/ex4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+class ArrayException : public std::exception
+{
+protected:
+    std::string msg;
+
+public:
+
+    ArrayException(const char* str) noexcept : msg(str) {}
+    ArrayException(const ArrayException& other) noexcept : msg(other.msg) {}
+
+    const char * what() const noexcept override
+    {
+        return msg.c_str();
+    }
+};
+
+int main()
+{
+    try
+    {
+        ArrayException("Main exception");
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    
+    // __ASSERT_TESTS__
+
+    return 0;
+}
